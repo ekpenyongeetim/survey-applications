b363d1b2ab8a3b7f6703a0376141e12620b5930a
ekpenyongeetim <ekpenyongeetim@gmail.com>
2024-07-21 22:11:36 +0100

    hamburger menu

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/fox-logo.svg";
 import { routes, socialLinks } from "./data";
+import menuIcon from "../assets/menu-icon.png"; // Use PNG for hamburger icon
+import closeIcon from "../assets/close-icon.png"; // Use PNG for close icon
 
 function MyHeader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <nav className="navbar no-print">
       <div className="nav-header">
         <img src={logo} className="nav-logo" alt="fox head logo" />
         <h1 className="nav--logo-text">Tech Fox</h1>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <img
+            src={isMenuOpen ? closeIcon : menuIcon}
+            alt="menu icon"
+            className="menu-icon"
+          />
+        </button>
       </div>
 
-      <ul className="nav-links" id="nav-links">
+      <ul
+        className={`nav-links ${isMenuOpen ? "show-menu" : ""}`}
+        id="nav-links"
+      >
         {routes.map((route) => (
           <li key={route.path}>
-            <Link to={route.path}>{route.label}</Link>
+            <Link to={route.path} onClick={() => setIsMenuOpen(false)}>
+              {route.label}
+            </Link>
           </li>
         ))}
       </ul>
